328703de1a87782654887444d728d04d531cc997
hariton1500 <hariton1500@gmail.com>
2022-04-05 23:01:57 +0300

    4

--- lib/screens/game2.dart
+++ lib/screens/game2.dart
@@ -4,6 +4,8 @@ import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:toytanks/client.dart';
 import 'package:toytanks/main.dart';
+import 'package:toytanks/screens/mainmenu.dart';
+import 'package:web_socket_channel/io.dart';
 
 ToyTanksGame _toyTanksGame = ToyTanksGame();
 
@@ -16,19 +18,47 @@ class GamePlay extends StatefulWidget {
 
 class _GamePlayState extends State<GamePlay> {
   
-  dynamic _connection;
+  late IOWebSocketChannel _connection;
+  dynamic res;
+  bool connectionEstablished = false;
+  int ticksWaiting = 0;
 
   @override
   void initState() {
     super.initState();
-    _connection = startConnection();
-    Timer.periodic(const Duration(milliseconds: 100), checkConnection());
+    res = startConnection();
+    Timer.periodic(const Duration(milliseconds: 100), (timer) => checkConnection(timer));
+  }
+
+  void checkConnection(Timer timer) {
+    //debugPrint((timer.tick % 10).toString());
+    //debugPrint(connectionEstablished.toString());
+    if (timer.tick >= 100) {
+      timer.cancel();
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const MainMenu()));
+    }
+    if (res is IOWebSocketChannel) {
+      _connection = res;
+    }
+    if (_connection.protocol != null) {
+      setState(() {
+        connectionEstablished = true;
+        timer.cancel();
+      });
+    }
+    setState(() {
+      ticksWaiting = timer.tick;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return GameWidget(
-      game: _toyTanksGame,
-    );
+    if (connectionEstablished) {
+      return GameWidget(game: _toyTanksGame,);
+    } else {
+      return Material(child: Center(
+          child: Text('(${(ticksWaiting / 10).ceil()}) connecting...'),
+          ));
+    }
   }
 }
